9dc29bcf1fbce766c89e67b61a7d8097e20dba7b
santhasaravanan2006 <santhasaravanan2006@gmail.com>
2025-12-17 16:11:32 +0530

    added requests import

--- python.py
+++ python.py
@@ -1,1 +1,6 @@
+import sys
+import requests
+from PyQt5.QtWidgets import (QApplication,QWidget,QLabel,
+                             QLineEdit,QPushButton,QVBoxLayout)
+from PyQt5.QtCore import Qt
 
